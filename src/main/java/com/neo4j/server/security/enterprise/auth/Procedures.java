60d788b78f1928a4f948a65f7378168ec137c501
Stefan Armbruster <stefan@armbruster-it.de>
2020-10-24 10:18:20 +0200

    extension for tracking spawned transactions cache for user roles

--- src/main/java/com/neo4j/server/security/enterprise/auth/Procedures.java
+++ src/main/java/com/neo4j/server/security/enterprise/auth/Procedures.java
@@ -1,38 +1,46 @@
 package com.neo4j.server.security.enterprise.auth;  // since we use a package-scope class from Neo4j, we can't change package name
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import com.neo4j.kernel.enterprise.api.security.EnterpriseLoginContext;
 import com.neo4j.kernel.enterprise.api.security.EnterpriseSecurityContext;
 import org.neo4j.common.DependencyResolver;
 import org.neo4j.dbms.api.DatabaseManagementService;
-import org.neo4j.graphalgo.impl.util.PathImpl;
-import org.neo4j.graphdb.Direction;
-import org.neo4j.graphdb.Label;
-import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.Path;
-import org.neo4j.graphdb.Relationship;
-import org.neo4j.graphdb.RelationshipType;
 import org.neo4j.graphdb.Transaction;
+import org.neo4j.internal.helpers.collection.Iterators;
 import org.neo4j.internal.kernel.api.security.AuthSubject;
 import org.neo4j.internal.kernel.api.security.AuthenticationResult;
 import org.neo4j.kernel.api.KernelTransaction;
 import org.neo4j.kernel.impl.coreapi.InternalTransaction;
 import org.neo4j.kernel.internal.GraphDatabaseAPI;
 import org.neo4j.logging.Log;
+import org.neo4j.plugins.impersonate.TransactionClosingExtensionFactory;
 import org.neo4j.procedure.Context;
 import org.neo4j.procedure.Name;
 import org.neo4j.procedure.Procedure;
 
-import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
-import java.util.stream.Collectors;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 
 import static org.neo4j.configuration.GraphDatabaseSettings.SYSTEM_DATABASE_NAME;
 
 public class Procedures {
+
+    private static Cache<String, Set<String>> userRolesCache = Caffeine.newBuilder()
+            .expireAfterWrite(5, TimeUnit.MINUTES)
+            .maximumSize(100)
+            .build();
+
+    private static Cache<String, String> defaultDatabaseCache = Caffeine.newBuilder()
+            .expireAfterWrite(10, TimeUnit.MINUTES)
+            .initialCapacity(1)
+            .maximumSize(1)
+            .build();
 
     @Context
     public DependencyResolver resolver;
@@ -47,53 +55,55 @@ public class Procedures {
     public Log log;
 
     @Procedure( name = "impersonate" )
-    public Stream<MapResult> sudo(@Name( "username" ) String username, @Name("cypher") String cypher, @Name("params")Map<String,Object> params)
+    public Stream<MapResult> impersonate(@Name( "username" ) String username, @Name("cypher") String cypher, @Name("params")Map<String,Object> params)
     {
         DatabaseManagementService dbms = resolver.resolveDependency( DatabaseManagementService.class );
-
-        // Starting a transaction on this database will escape security
-//        GraphDatabaseAPI database = (GraphDatabaseAPI) dbms.database( "database name goes here" );
-
-        MultiRealmAuthManager authManager = resolver.resolveDependency( MultiRealmAuthManager.class );
-        // Need to manually resolve roles here since we can't get the securityManager that exists in the authManager
         GraphDatabaseAPI system = (GraphDatabaseAPI) dbms.database(SYSTEM_DATABASE_NAME);
-        Set<String> roleNames = new TreeSet<>();
-        String defaultDatabaseName;
-        Collection<String> roles = system.executeTransactionally("show roles with users where member = $user", Collections.singletonMap("user", username), result -> result.stream().map( m -> m.get("role").toString()).collect(Collectors.toSet()));
 
-        try ( Transaction tx = system.beginTx() )
-        {
-            Node userNode = tx.findNode( Label.label( "User" ), "name", username );
-            if ( userNode != null )
-            {
-                boolean suspended = (boolean) userNode.getProperty( "suspended" );
-                final Iterable<Relationship> rels = userNode.getRelationships( Direction.OUTGOING, RelationshipType.withName( "HAS_ROLE" ) );
-                rels.forEach( rel -> roleNames.add( (String) rel.getEndNode().getProperty( "name" ) ) );
-            }
-            Node defaultDatabase = tx.findNode( Label.label( "Database" ), "default", true );
-            defaultDatabaseName = (String) defaultDatabase.getProperty( "name" );
+        Set<String> roleNames;
+        String defaultDatabaseName;
+
+        // Need to manually resolve roles here since we can't get the securityManager that exists in the authManager
+        try (Transaction tx = system.beginTx()) {
+            roleNames = userRolesCache.get(username, s -> {
+
+                Map<String, Object> user = Iterators.single(tx.execute("show users where user=$user", Collections.singletonMap("user", username)));
+
+                boolean suspended = (boolean) user.get("suspended");
+                if (suspended) {
+                    throw new IllegalStateException("cannot impersonate a suspended account : " + username);
+                }
+                List<String> roles = (List<String>) user.get("roles");
+                log.info("caching user roles for " + username + " = " + roles);
+                return Set.copyOf(roles);
+            });
+            defaultDatabaseName = defaultDatabaseCache.get("default",
+                    s -> (String) Iterators.single(tx.execute("show databases where default")).get("name")
+            );
             tx.commit();
         }
 
-        InternalTransaction internalTransaction = database.beginTransaction(KernelTransaction.Type.EXPLICIT, new SudoLoginContext(authManager, defaultDatabaseName, username, roleNames));
+        MultiRealmAuthManager authManager = resolver.resolveDependency( MultiRealmAuthManager.class );
+        InternalTransaction impersonateTransaction = database.beginTransaction(
+                KernelTransaction.Type.EXPLICIT,
+                new ImpersonatedLoginContext(authManager, defaultDatabaseName, username, roleNames)
+        );
+        log.debug("opened transaction for impersonate " + impersonateTransaction);
 
-        return internalTransaction.execute(cypher, params).stream().map(MapResult::new).onClose(() -> {
-            try {
-                internalTransaction.commit();
-            } catch (Exception e) {
-                log.error(e.getMessage(), e);
-            }
-        });
+        // from 4.2 onwards we can use transaction.
+        TransactionClosingExtensionFactory.TransactionClosing transactionClosing = resolver.resolveDependency(TransactionClosingExtensionFactory.TransactionClosing.class);
+        transactionClosing.takeCareOf((InternalTransaction) transaction, impersonateTransaction);
+
+        return impersonateTransaction.execute(cypher, params).stream().map(MapResult::new);
     }
 
-
-    private static class SudoLoginContext implements EnterpriseLoginContext
+    private static class ImpersonatedLoginContext implements EnterpriseLoginContext
     {
         private final MultiRealmAuthManager authManager;
         private final String username;
         private final String defaultDatabase;
         private final Set<String> roles;
-        public SudoLoginContext( MultiRealmAuthManager authManager, String defaultDatabase, String username, Set<String> roles )
+        public ImpersonatedLoginContext(MultiRealmAuthManager authManager, String defaultDatabase, String username, Set<String> roles )
         {
             this.authManager = authManager;
             this.defaultDatabase = defaultDatabase;
@@ -170,23 +180,7 @@ public class Procedures {
 
         public MapResult(Map<String, Object> map) {
             this.map = map;
-            // rebind node, relationship and path instances to outer transaction
-            map.entrySet().forEach(e -> {
-                Object val = e.getValue();
-                if (val instanceof Node) {
-                    Node n = (Node) val;
-                    map.put(e.getKey(), transaction.getNodeById(n.getId()));
-                } else if (val instanceof Relationship) {
-                    Relationship r = (Relationship) val;
-                    map.put(e.getKey(), transaction.getNodeById(r.getId()));
-                } else if (val instanceof Path) {
-                    Path p = (Path) val;
-                    Node startNode = transaction.getNodeById(p.startNode().getId());
-                    PathImpl.Builder builder = new PathImpl.Builder(startNode);
-                    p.relationships().forEach( r -> builder.push(transaction.getRelationshipById(r.getId())));
-                    map.put(e.getKey(), builder.build());
-                }
-            });
         }
     }
+
 }
---
+++ src/main/java/org/neo4j/plugins/impersonate/TransactionClosingExtensionFactory.java
@@ -0,0 +1,81 @@
+package org.neo4j.plugins.impersonate;
+
+import org.neo4j.kernel.extension.ExtensionFactory;
+import org.neo4j.kernel.extension.ExtensionType;
+import org.neo4j.kernel.extension.context.ExtensionContext;
+import org.neo4j.kernel.impl.coreapi.InternalTransaction;
+import org.neo4j.kernel.lifecycle.Lifecycle;
+import org.neo4j.kernel.lifecycle.LifecycleAdapter;
+import org.neo4j.logging.Log;
+import org.neo4j.logging.internal.LogService;
+import org.neo4j.scheduler.JobHandle;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class TransactionClosingExtensionFactory extends ExtensionFactory<TransactionClosingExtensionFactory.Dependencies> {
+
+    public TransactionClosingExtensionFactory() {
+        super(ExtensionType.GLOBAL, "transactionClosing");
+    }
+
+    @Override
+    public Lifecycle newInstance(ExtensionContext context, Dependencies dependencies) {
+        return new TransactionClosing(dependencies);
+    }
+
+    public interface Dependencies {
+        LogService log();
+    }
+
+    public static class TransactionClosing extends LifecycleAdapter {
+
+        private final Dependencies dependencies;
+        private JobHandle<?> jobHandle;
+        private Map<InternalTransaction, InternalTransaction> transactionMap;
+        private ScheduledExecutorService scheduledExecutorService;
+
+        public TransactionClosing(Dependencies dependencies) {
+            this.dependencies = dependencies;
+            transactionMap = new ConcurrentHashMap<>();
+        }
+
+        @Override
+        public void start() throws Exception {
+
+            Log log = dependencies.log().getInternalLog(TransactionClosingExtensionFactory.class);
+
+            scheduledExecutorService = Executors.newScheduledThreadPool(1);
+            scheduledExecutorService.scheduleAtFixedRate(() -> {
+                try {
+                    log.info("processing transaction list");
+                    transactionMap.keySet().stream().filter(t -> !t.isOpen()).forEach( source -> {
+                        InternalTransaction target = transactionMap.remove(source);
+                        if (source.terminationReason().isEmpty()) {
+                            target.commit();
+                        } else {
+                            target.rollback();
+                        }
+                        target.close();
+                    });
+                } catch (Exception e) {
+                    log.error(e.getMessage(), e);
+                }
+            }, 5, 5, TimeUnit.SECONDS);
+        }
+
+        @Override
+        public void stop() throws Exception {
+            scheduledExecutorService.shutdown();
+            scheduledExecutorService.awaitTermination(10, TimeUnit.SECONDS);
+        }
+
+        public void takeCareOf(InternalTransaction source, InternalTransaction target) {
+            transactionMap.put(source, target);
+        }
+    }
+
+}
